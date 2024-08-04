6825c2bc55bde334c31d9591175d6fb6bdd5bad3
JsDev02 <sebas.mosquera2002@outlook.com>
2024-08-03 19:04:24 -0500

    EndPoints Roles

---
+++ MicroApi.Seguridad.Domain/Models/PersonalModulo/CrearPersonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroApi.Seguridad.Domain.Models.PersonalModulo
+{
+    public class CrearPersonal
+    {
+        [Required]
+        public int Doc_ChaLog { get; set; } // Documento del ChairaLogin
+
+        [Required]
+        public int Id_RolModulo { get; set; } // ID del RolModulo
+    }
+}
