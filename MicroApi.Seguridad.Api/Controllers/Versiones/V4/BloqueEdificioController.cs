ad70a600033e53b4faf63033e6e5a3060e3e2e6d
Johan Sebastian Mosquera Munar <joh.mosquera@udla.edu.co>
2024-08-05 09:39:39 -0500

    Select a Bloque y edificios

---
+++ MicroApi.Seguridad.Api/Controllers/Versiones/V4/BloqueEdificioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroApi.Seguridad.Api.Controllers.Versiones.V4
+{
+    [Route("api/v4/[controller]")]
+    [ApiController]
+    public class BloqueEdificioController : ControllerBase
+    {
+        private readonly ModelContextSQL _context;
+
+        public BloqueEdificioController(ModelContextSQL context)
+        {
+            _context = context;
+        }
+
+        // GET api/v3/persona/salas
+        [HttpGet("Bloque o edificio")]
+        public async Task<IActionResult> GetBloqueEdificio()
+        {
+            var BloqueEdificio = await _context.BloqueEdificios
+                .Select(s => new
+                {
+                    s.Id_BloqEdi,
+                    s.Nom_BloqEdi
+
+                })
+                .ToListAsync();
+
+            if (BloqueEdificio == null || !BloqueEdificio.Any())
+            {
+                return NotFound("No se encontraron bloques o edificios.");
+            }
+
+            return Ok(BloqueEdificio);
+        }
+    }
+}
