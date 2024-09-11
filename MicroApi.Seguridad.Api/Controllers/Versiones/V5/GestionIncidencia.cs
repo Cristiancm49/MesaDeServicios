f85a89e151ca6120ed5914a5b6b449b9fc1ee154
JsDev02 <sebas.mosquera2002@outlook.com>
2024-09-11 09:28:47 -0500

    Usuarios por asignar

---
+++ MicroApi.Seguridad.Api/Controllers/Versiones/V5/GestionIncidencia.cs
@@ -0,0 +1,67 @@
+﻿using MicroApi.Seguridad.Domain.Models.Incidencia;
+using MicroApi.Seguridad.Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MicroApi.Seguridad.Data.Context;
+using Microsoft.SqlServer.Server;
+
+namespace MicroApi.Seguridad.Api.Controllers.Versiones.V5
+{
+    [Route("api/v5/[controller]")]
+    [ApiController]
+    public class GestionIncidencia : ControllerBase
+    {
+
+        private readonly ModelContextSQL _context;
+
+        public GestionIncidencia(ModelContextSQL context)
+        {
+            _context = context;
+        }
+        [HttpGet("UsuariosConIncidenciasAsignadas")]
+        public async Task<IActionResult> GetUsuariosConIncidenciasAsignadas()
+        {
+            var usuariosConIncidencias = await _context.Usuarios
+                .Join(
+                    _context.Contratos,
+                    u => u.Cont_Id,
+                    c => c.Cont_Id,
+                    (u, c) => new { Usuario = u, Contrato = c }
+                )
+                .Join(
+                    _context.PersonasGenerales,
+                    uc => uc.Contrato.PeGe_Id,
+                    p => p.PeGe_Id,
+                    (uc, p) => new { uc.Usuario, uc.Contrato, Persona = p }
+                )
+                .GroupJoin(
+                    _context.IncidenciasTrazabilidad.Where(t => t.InTrEs_Id == 5),
+                    uc => uc.Usuario.Usua_Id,
+                    t => t.Usua_Id,
+                    (uc, trazabilidades) => new { uc.Usuario, uc.Contrato, uc.Persona, IncidenciasActivas = trazabilidades }
+                )
+                .Join(
+                    _context.UsuariosRoles,
+                    u => u.Usuario.UsRo_Id,
+                    r => r.UsRo_Id,
+                    (u, r) => new
+                    {
+                        NombreCompleto = $"{u.Persona.PeGe_PrimerNombre} {u.Persona.PeGe_SegundoNombre} {u.Persona.PeGe_PrimerApellido} {u.Persona.PeGe_SegundoApellido}",
+                        RolNombre = r.UsRo_Nombre,
+                        IncidenciasActivas = u.IncidenciasActivas.Count(),
+                        u.Usuario.Usua_PromedioEvaluacion
+                    }
+                )
+                .ToListAsync();
+
+            if (usuariosConIncidencias == null || !usuariosConIncidencias.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(usuariosConIncidencias);
+        }
+
+    }
+}
